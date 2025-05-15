9704b3218728d7d28b753a8385fd108dbcb6e937
Ziaul Hoda <zhoda315@gmail.com>
2025-05-15 12:28:00 +0530

    bug fixes index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 app.get("/", (req,res) => {
-    res.status(204).json({'message':'Welcome to the root user'})
+    res.json({'message':'Welcome to the root user'})
 })
 
 app.get("/about", (req,res) => {
-    res.status(204).json({"Project":"tutorial_deployment","backend":"true"})
+    res.json({"Project":"tutorial_deployment","backend":"true"})
+})
+
+// POST /info {username,password} -> response {username,password}
+app.post("/info", (req,res) => {
+    const {username, password} = req.body;
+    res.json({username:username,password:password});
 })
 app.listen(3000 ,() => {
     console.log("we are listening to a port");
